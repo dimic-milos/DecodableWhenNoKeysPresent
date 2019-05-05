96453db7065b42f66479a6a244993ccd28b2d912
Milos Dimic <dimic.milos@icloud.com>
2019-05-05 11:29:13 +0200

    Initial implementation;

--- DecodableWhenNoKeysPresent/ViewController.swift
+++ DecodableWhenNoKeysPresent/ViewController.swift
@@ -10,11 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let data = """
+    [{
+        "id": 1,
+        "array": [
+            [
+                "Sunny",
+                13.5,
+                70
+            ],
+            [
+                "Cloudy",
+                2.5,
+                10
+            ]
+        ]
+    },
+    {
+            "id": 2,
+            "array": [
+                [
+                    "Snow",
+                    -14,
+                    90
+                ],
+                [
+                    "Rain",
+                    7,
+                    100
+                ]
+            ]
+        }]
+    """.data(using: .utf8)!
+    
+    struct Foo: Decodable {
+        let id: Int
+        let array: [Bar]
+    }
+    
+    struct Bar: Decodable {
+        
+        let weatherDescription: String
+        let temperature: Double
+        let humidity: Int
+        
+        init(from decoder: Decoder) throws {
+            var container = try decoder.unkeyedContainer()
+            weatherDescription = try container.decode(String.self)
+            temperature =  try container.decode(Double.self)
+            humidity = try container.decode(Int.self)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        decode()
     }
 
+    func decode() {
+        do {
+             let foos = try JSONDecoder.init().decode([Foo].self, from: data)
+            print(foos)
+        } catch {
+            print(error)
+        }
+       
+        
+        
+    }
 
 }
 
